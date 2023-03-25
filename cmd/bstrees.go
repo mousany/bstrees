6d51a51225013b1de3993808cfbdd428e03a37c5
yanglinshu <nicolasvandarkholme@outlook.com>
2023-03-26 04:16:37 +0800

    Finish rework of treap tree

--- cmd/bstrees.go
+++ cmd/bstrees.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bstrees/pkg/trees/fhq"
+	"bstrees/pkg/trees/treap"
 	"bufio"
 	"fmt"
 	"os"
@@ -35,7 +35,7 @@ func ReadWithPanic(gin *bufio.Reader) int {
 }
 
 func main() {
-	tree := fhq.New[int]()
+	tree := treap.New[int]()
 	gin := bufio.NewReader(os.Stdin)
 	n := ReadWithPanic(gin)
 	for i := 0; i < n; i++ {
---
+++ pkg/trees/treap/balance.go
@@ -0,0 +1,21 @@
+package treap
+
+import "bstrees/internal/order"
+
+func leftRotate[T order.Ordered](root *treapTreeNode[T]) *treapTreeNode[T] {
+	right := root.right
+	root.right = right.left
+	right.left = root
+	root.Update()
+	right.Update()
+	return right
+}
+
+func rightRotate[T order.Ordered](root *treapTreeNode[T]) *treapTreeNode[T] {
+	left := root.left
+	root.left = left.right
+	left.right = root
+	root.Update()
+	left.Update()
+	return left
+}
